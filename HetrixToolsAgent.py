10c427bc8b2e448a5e2bc916c0d2b37374fd5814
HetrixTools <admin@hetrixtools.com>
2018-05-07 11:46:28 +0300

    Initial

---
+++ HetrixToolsAgent.py
@@ -0,0 +1,79 @@
+import os
+import time
+import sys
+
+import win32serviceutil
+import win32service
+import win32event
+import servicemanager
+import socket
+
+import resource_monitor as monitor
+
+SERVICE_NAME = "HetrixToolsAgent"
+SERVICE_DISPLAY_NAME = "HetrixTools Agent"
+
+class AppServerSvc (win32serviceutil.ServiceFramework):
+    _svc_name_ = SERVICE_NAME
+    _svc_display_name_ = SERVICE_DISPLAY_NAME
+
+    def __init__(self, args):
+        win32serviceutil.ServiceFramework.__init__(self, args)
+        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
+        socket.setdefaulttimeout(60)
+
+    def SvcStop(self):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        win32event.SetEvent(self.hWaitStop)
+
+    def SvcDoRun(self):
+        self.ReportServiceStatus(win32service.SERVICE_START_PENDING)
+        self.ReportServiceStatus(win32service.SERVICE_RUNNING)
+        servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
+                              servicemanager.PYS_SERVICE_STARTED,
+                              (self._svc_name_, ''))
+        SID = win32serviceutil.GetServiceCustomOption(SERVICE_NAME, 'sid')
+        servicemanager.LogInfoMsg('SID {}'.format(SID))
+        while True:
+            data = monitor.gather_data(SID)
+            servicemanager.LogInfoMsg('data {}'.format(data))
+            if win32event.WaitForSingleObject(self.hWaitStop, 100) == win32event.WAIT_OBJECT_0:
+                break
+
+def instart ():
+    module_path = os.path.splitext(os.path.realpath(__file__))[0]
+    class_name = "{}.{}".format(module_path, AppServerSvc.__name__)
+    win32serviceutil.InstallService(class_name,
+                                    SERVICE_NAME, SERVICE_DISPLAY_NAME,
+                                    startType=win32service.SERVICE_AUTO_START,
+                                    )
+    print('Install OK')
+    win32serviceutil.StartService(SERVICE_NAME)
+
+if __name__ == '__main__':
+    is_installed = True
+    SID = win32serviceutil.GetServiceCustomOption(SERVICE_NAME, 'sid')
+    if SID is None:
+        # SID is missing, ask for it from user
+        prompt_string = """
+        ########################################
+        Hint: You can paste into this window
+        by right clicking the window header
+        and selecting 'Edit' > 'Paste'
+        ########################################
+        Please enter your Server ID:
+
+        """
+        SID = raw_input(prompt_string)
+        win32serviceutil.SetServiceCustomOption(SERVICE_NAME, 'sid', SID)
+        is_installed = False
+
+    if len(sys.argv) == 1:
+        if not is_installed:
+            instart()
+        else:
+            servicemanager.Initialize()
+            servicemanager.PrepareToHostSingle(AppServerSvc)
+            servicemanager.StartServiceCtrlDispatcher()
+    else:
+        win32serviceutil.HandleCommandLine(AppServerSvc)
---
+++ resource_monitor.py
@@ -0,0 +1,129 @@
+import time
+import base64
+
+import psutil
+import requests
+import wmi
+from win32com.client import GetObject
+import pythoncom
+
+####################
+# Settings
+####################
+# Server version (do not edit)
+VERSION = '1.5.2'
+URL = 'https://sm.hetrixtools.com'
+
+####################
+# Functions
+####################
+def get_win_version():
+    c = wmi.WMI()
+    for os_record in c.Win32_OperatingSystem():
+        win = os_record.Caption
+        sp = os_record.ServicePackMajorVersion
+    win = win.replace('Microsoft ','')
+    if sp and sp > 0:
+        win += 'SP'+str(sp)
+    return win
+
+
+def get_cpu_type():
+    root_winmgmts = GetObject("winmgmts:root\cimv2")
+    cpus = root_winmgmts.ExecQuery("Select * from Win32_Processor")
+    return cpus[0].Name
+
+def gather_data(server_id):
+    pythoncom.CoInitialize()
+    # Initial Network Stats
+    net_stats1 = psutil.net_io_counters()
+    
+    # Time
+    sec = int(time.strftime("%S"))
+    sleep = 60 - sec
+
+    # CPU Usage
+    cpu_usage = psutil.cpu_percent(interval=sleep)
+
+    # Secondary Network Stats
+    net_stats2 = psutil.net_io_counters()
+
+    # OS
+    os_name = base64.b64encode(get_win_version())
+
+    # Uptime
+    uptime = int(time.time() - psutil.boot_time())
+
+    # CPU Model
+    cpu_model = base64.b64encode(get_cpu_type())
+
+    # CPU Speed
+    cpu_speed = int(psutil.cpu_freq().current)
+
+    # CPU Cores
+    cpu_cores = psutil.cpu_count()
+
+    # IOWait
+    cpu_iowait = 0
+
+    # RAM Info
+    ram_info = psutil.virtual_memory()
+
+    # RAM Size
+    ram_size = int(ram_info.total/1000)
+
+    # RAM Usage
+    ram_usage = ram_info.percent
+
+    # Swap Info
+    swap_info = psutil.swap_memory()
+
+    # Swap Size
+    swap_size = int(swap_info.total/1000)
+
+    # Swap Usage
+    swap_usage = swap_info.percent
+
+    # Disks
+    disks = psutil.disk_partitions()
+    all_disk_data = []
+    for disk in disks:
+        if disk.fstype:
+            disk_usage = psutil.disk_usage(disk.mountpoint)
+            disk_data = ','.join([disk.mountpoint, str(disk_usage.total), str(disk_usage.used)])
+            all_disk_data.append(disk_data)
+
+    disk_str = '{};'.format(';'.join(all_disk_data))
+    disks = base64.b64encode(disk_str)
+
+    # Network
+    rx = int((net_stats2.bytes_recv - net_stats1.bytes_recv)/sleep)
+    tx = int((net_stats2.bytes_sent - net_stats1.bytes_sent)/sleep)
+
+    # Post Data
+    data = [
+        os_name,
+        str(uptime),
+        cpu_model,
+        str(cpu_speed),
+        str(cpu_cores),
+        str(cpu_usage),
+        str(cpu_iowait),
+        str(ram_size),
+        str(ram_usage),
+        str(swap_size),
+        str(swap_usage),
+        disks,
+        str(rx),
+        str(tx)
+    ]
+    post_data = '{}|'.format('|'.join(data))
+
+    payload = {'v': VERSION, 'a': '2', 's': server_id, 'd': post_data}
+
+    # Post the collected data
+    try:
+        requests.post(URL, data=payload)
+    except:
+        # Can't send data, try again next minute
+        pass
